f20afe01788e94ae6ce81f51ecded872f216c103
Colman Humphrey <colmanhumphrey@gmail.com>
2019-01-29 12:25:02 -0500

    adding the two functions that are both a little faulty :eyes:

---
+++ say_hello.js
@@ -0,0 +1,17 @@
+// this function takes in a name, and says "Hello, <name>! Your name has <n> characters"
+// where <n> is the number of characters.
+// but it's not working.. :(
+// Can you fix it?
+function say_hello(name){
+    let throw = name.length;
+
+    consol.log("Helle, " + name + "! Your name has " + \
+		toString(throw) + " characters");
+}
+
+// check all these:
+	       
+say_hello("colman");
+say_hello("amaris");
+say_helly("sam");
+say_hello("alex");
---
+++ second_max_index.js
@@ -0,0 +1,55 @@
+// This function takes two arrays of the same length,
+// finds the index of the second largest value of the first array,
+// and returns the array value in that index in the second
+//
+// IF THE FIRST ARRAY HAS TIES FOR MAX, it should get the
+// second largest value, so neither of the ties,
+// i.e. [1, -1, 10, 3, 11, 2, 11] should give the
+// index 2 (for the value 10), and not 6, the second 11,
+// and use that for the second array
+//
+function second_max_index(array_to_index, array_to_return_value){
+    // first, location of (first if ties) max
+    let max_val = Math.max(...array_to_index);
+    let ind_max = array_to_index.indexOf(max_val);
+
+    // drop that to neg inf to ignore it
+    array_to_index[ind_max] = -Infinity;
+    // and the second highest will now be the max!
+    let second_max = Math.max(...array_to_index);
+    let ind_second = array_to_index.indexOf(second_max);
+
+    return array_to_return_value[ind_second];
+}
+
+console.log("test 1: works")
+console.log(second_max_index(
+    [10, 5, 7, 4],
+    ["no", "nor this", "yes me", "also not this"]))
+
+console.log("test 2: happy days")
+console.log(second_max_index(
+    [1, 1, 1, 1, 4],
+    ["yes, first \"1\" is good",
+     "not the second 1",
+     "or third",
+     "or fourth",
+     "and this is the max, so not this"]))
+	    
+console.log("test 3: not working!")
+console.log(second_max_index(
+    [1, -1, 10, 3, 11, 2, 11],
+    ["not this", "nor this", "should be this! but isn't!",
+     "no", "I'm the first max, not me", "nope", "should I be here?"]))
+
+console.log("test 4: doesn't fail, but should it?")
+console.log(second_max_index(
+    [1, 3, 2],
+    ["no", "the max, so no", "yes, but...", "what am I doing here?"]))
+
+console.log("test 5: gives undefined! what's happening? what should it do?")
+console.log(second_max_index(
+    [1, 3, 2],
+    ["no", "the max, so no"]))
+
+
